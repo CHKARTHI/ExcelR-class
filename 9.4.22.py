99c00b7694388834fe8ad1209ea9468ef5c3401a
CHKARTHI <117970171+CHKARTHI@users.noreply.github.com>
2023-01-25 11:48:48 +0530

    Add files via upload

---
+++ 9.4.22.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep  4 15:30:56 2022
+
+@author: DELL
+"""
+
+import pandas as pd
+
+import numpy as np
+from sklearn.linear_model import LinearRegression
